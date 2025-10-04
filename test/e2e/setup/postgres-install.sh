abbff1c8bc75af1ce1f1169acc71d6f4b9d4cfc4
whg517 <huagang517@126.com>
2025-10-04 10:55:28 +0800

    test: refactor e2e to replace bitnami images

--- test/e2e/setup/postgres-install.sh
+++
@@ -1,9 +0,0 @@
-# Get the directory where this script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-helm upgrade --install superset \
-  --version=12.5.6 \
-  --wait \
-  --namespace $NAMESPACE \
-  -f "${SCRIPT_DIR}/helm-bitnami-postgresql-values.yaml" \
-  --repo https://charts.bitnami.com/bitnami postgresql \
